874ab3fdee4a83b93d0e838a26bb7b7722c3e29e
Ana Filipa <anafilipa737@gmail.com>
2024-02-08 14:26:40 +0000

    Iteration 1 - I think I have an error on line 3 of App.jsx

---
+++ src/components/ProductRow.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function ProductRow() {
+
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+  export default ProductRow;
---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductRow from "./ProductRow";
+
+function ProductTable() {
+
+
+  return (
+    <div>
+      <ProductRow />
+    </div>
+  );
+}
+
+export default ProductTable;
